b3e95d2d579d44bf816689c887a4215c4c45311f
emmanuel kinfie <maimoe@yahoo.com>
2023-03-17 15:57:50 -0700

    coding in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - the start of the c programm that print aline to standard erro
+*Return: returns 1
+*/
+int main(void)
+{
+	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19 \n", stdout);
+	return (1);
+}
