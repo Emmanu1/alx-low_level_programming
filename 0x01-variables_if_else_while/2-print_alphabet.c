6c60ce64a8c8fa647fd8f4072c0636e7c43e3f53
emmanuel kinfie <maimoe@yahoo.com>
2023-03-17 19:05:51 -0700

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - start
+ *Return:returns 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
