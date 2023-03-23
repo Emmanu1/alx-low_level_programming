3b0b76245d3f1e7445d0354e4a912333d6a77dec
emmanuel kinfie <maimoe@yahoo.com>
2023-03-22 17:48:15 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet - Print the alphabet, lowercase
+ *Return: always zero
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i = 122; 1++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
